7c50a1ef279754abe4a979b0b5e330a40239dcee
ppgamedevs <ppgamedevs@gmail.com>
2025-10-12 13:12:29 +0000

    feat: set light mode as default theme

--- imob-ro/src/components/theme-provider.tsx
+++ imob-ro/src/components/theme-provider.tsx
@@ -8,7 +8,7 @@ type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem {...props}>
+    <NextThemesProvider attribute="class" defaultTheme="light" enableSystem {...props}>
       {children}
     </NextThemesProvider>
   );
